7e7185c012a0a4e34be62998c6bd9ec1ac132244
Stefan <bogosavljevic.1911@gmail.com>
2018-10-08 20:20:27 +0200

    first commit

---
+++ HelloAndroid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            TextView txtNumber = FindViewById<TextView>(Resource.Id.numberTxt);
+
+            FindViewById<Button>(Resource.Id.changeBt).Click += (o, e) =>
+            {
+                int number = int.Parse(txtNumber.Text);
+                txtNumber.Text = (++number).ToString();
+            };
+
+            
+        }
+    }
+}
+
